4cef16c90be9bc4c2764a9adb42fad90c95f6e2d
CDE90 <cdeyolo@outlook.com>
2021-11-21 10:43:46 +0000

    Support for multiple admin roles

--- cogs/admin.py
+++ cogs/admin.py
@@ -9,28 +9,28 @@ class Admin(commands.Cog):
         self.client = client
 
     @slash_command(guild_ids=config["guild_ids"], default_permission=False)
-    @permissions.has_role(config["admin_role"])
+    @permissions.has_role(*config["admin_roles"])
     async def clear(self, ctx, amount: int = 5):
         """Clears an amount of messages"""
         await ctx.channel.purge(limit=amount)
         await ctx.respond(f"{amount} messages deleted!", delete_after=3)
 
     @slash_command(guild_ids=config["guild_ids"], default_permission=False)
-    @permissions.has_role(config["admin_role"])
+    @permissions.has_role(*config["admin_roles"])
     async def kick(self, ctx, member: discord.Member, *, reason: str = None):
         """Kicks a member from the server"""
         await member.kick(reason=reason)
         await ctx.respond(f"{member} was kicked!", delete_after=3)
 
     @slash_command(guild_ids=config["guild_ids"], default_permission=False)
-    @permissions.has_role(config["admin_role"])
+    @permissions.has_role(*config["admin_roles"])
     async def ban(self, ctx, member: discord.Member, *, reason: str = None):
         """Bans a member from the server"""
         await member.ban(reason=reason)
         await ctx.respond(f"{member} was banned!", delete_after=3)
 
     @slash_command(guild_ids=config["guild_ids"], default_permission=False)
-    @permissions.has_role(config["admin_role"])
+    @permissions.has_role(*config["admin_roles"])
     async def unban(self, ctx, member: discord.Member, *, reason: str = None):
         """Unbans a member from the server"""
         await member.unban(reason=reason)
